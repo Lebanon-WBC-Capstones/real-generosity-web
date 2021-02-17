e4826e5d3d393465586d6aec52a51658c9444a14
sally-al-sardouk <sally.sardouk98@gmail.com>
2021-02-17 20:16:10 +0200

    Feat: Header-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
