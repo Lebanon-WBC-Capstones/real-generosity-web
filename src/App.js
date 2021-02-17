0f15288c3635b77cb64f7032ecc46a1173da4c2f
Abdulrahman Y. idlbi <adlogi@alum.mit.edu>
2021-02-17 10:08:46 +0300

    refactor: rearrange components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import './App.css';
 
 function App() {
   return (
