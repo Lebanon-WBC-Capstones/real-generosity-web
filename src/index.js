2253089fd1be16bd8ffd683893b2f9613241edf9
Skia69 <Skiastep@gmail.com>
2021-02-18 13:31:09 +0200

    bug: add a missing import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './i18n';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
