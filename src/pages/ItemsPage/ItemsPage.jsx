0db16ce3f3dedd9bcefc54f3db73f2779de59cfb
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-02-18 17:00:08 +0200

    fix: fix a bug

--- src/pages/ItemsPage/ItemsPage.jsx
+++ src/pages/ItemsPage/ItemsPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Container, Flex,Box } from '@chakra-ui/react';
-//import ItemsMap from '../../components/ItemsMap/ItemsMap';
+import ItemsMap from '../../components/ItemsMap/ItemsMap';
 
 const ItemsPage = () => {
   return (
-    // <Container maxwidth="1080px">
-    //   <Flex justify="space-between">
-    //     <ItemsMap />
-    //   </Flex>
-    // </Container>
+  
     <Container my="45px" maxW="1080px">
     <Box mb="45px"></Box>
     <Box mb="45px"></Box>
@@ -18,7 +14,7 @@ const ItemsPage = () => {
   
        </Box>
        <Box  w="50%">
-    
+       <ItemsMap />
        </Box>
     </Flex>
  
