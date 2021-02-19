f101b69f4026bde20a896333643302e183384790
Skia69 <Skiastep@gmail.com>
2021-02-19 20:11:29 +0200

    refactor: refactor the layout

--- src/components/Descriptionpage/Description.jsx
+++ src/components/Descriptionpage/Description.jsx
@@ -1,81 +1,60 @@
 import React from 'react';
-import {
-  Flex,
-  Box,
-  VStack,
-  HStack,
-  Text,
-  Button,
-  Badge,
-} from '@chakra-ui/react';
+import { Flex, Box, HStack, Text, Button, Badge } from '@chakra-ui/react';
 import { MapPin, Edit2, ArrowLeft, AlertCircle, Trash2 } from 'react-feather';
 
 const Description = () => {
   return (
-    <Box py="10" my="50" fontSize={18} fontWeight={600}>
-      <VStack>
-        <HStack spacing={80}>
-          <Button variant="ghost">Details</Button>
-          <Button color="gray" variant="ghost">
-            Request
-          </Button>
-        </HStack>
-        <hr width="50%" />
-        <HStack spacing={80}>
-          <Button variant="ghost">
-            <ArrowLeft size={15} />
-            items
-          </Button>
-          <HStack color="gray" fontSize={12}>
-            <Button variant="ghost">
-              <Edit2 size={15} />
-              Edit
-            </Button>
-          </HStack>
-        </HStack>
-      </VStack>
-      <VStack>
-        <Box my="40px" bg="gray.200" mr={370} size="lg">
-          <Badge px="5">Clothes</Badge>
-        </Box>
-        <Box
-          color="black"
-          fontWeight="bold"
-          letterSpacing="wide"
-          fontSize="3xl"
-          mr={260}
-        >
-          items name
-          <HStack>
-            <MapPin color="green" size={15} />
-            <Text color="gray" fontSize="15px">
-              Tripoli,Mina
-            </Text>
-          </HStack>
-        </Box>
-        <Box mr={340} py="50px">
-          Description
-        </Box>
-
-        <Button mr={-20} colorScheme="green" width={400} size="sm">
+    <Flex d="column" px={28} maxW="50%" my="50" fontSize={18}>
+      <Flex justify="space-between">
+        <Button variant="ghost">Details</Button>
+        <Button color="gray" variant="ghost">
           Request
         </Button>
-
-        <HStack mr={-20} pt={50} spacing={300}>
-          <HStack>
-            <AlertCircle size={15} color="red" />
-            <Text color="red" fontSize={15}>
-              Report
-            </Text>
-          </HStack>
-          <HStack>
-            {' '}
-            <Trash2 size={15} />
-            <Text fontSize={15}>Delete</Text>
-          </HStack>
+      </Flex>
+      <hr width="100%" />
+      <Flex justify="space-between">
+        <Button leftIcon={<ArrowLeft size={15} />} variant="ghost">
+          items
+        </Button>
+        <HStack color="gray" fontSize={12}>
+          <Button leftIcon={<Edit2 size={15} />} variant="ghost">
+            Edit
+          </Button>
         </HStack>
-      </VStack>
-    </Box>
+      </Flex>
+
+      <Badge my="40px" bg="gray.100" fontSize="1em" py="1" px="5">
+        Clothes
+      </Badge>
+
+      <Box color="black" fontWeight="bold" letterSpacing="wide" fontSize="3xl">
+        items name
+        <HStack>
+          <MapPin color="green" size={15} />
+          <Text color="gray" fontSize="15px">
+            Tripoli,Mina
+          </Text>
+        </HStack>
+      </Box>
+      <Box py="50px">Description</Box>
+
+      <Button colorScheme="green" w="100%" size="lg">
+        Request
+      </Button>
+
+      <Flex justify="space-between" pt={50}>
+        <Button
+          color="red"
+          leftIcon={<AlertCircle size={15} color="red" />}
+          variant="ghost"
+        >
+          Report
+        </Button>
+        <Button leftIcon={<Trash2 size={15} />} variant="ghost">
+          Delete
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
