92f4fee2d9e37a563a85cb14f21a2932716bcf82
Joseph <joseftaerrazi@gmai.com>
2021-02-21 13:24:31 +0200

    Fill app.js routes Fixes #61

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
 import ItemsPage from './pages/ItemsPage';
 
 function App() {
@@ -10,12 +11,14 @@ function App() {
           <Switch>
             <Route exact path="/">
               {/* Home page goes here  */}
+              <HomePage />
             </Route>
-            <Route exact path="/auth/login">
+            <Route exact path="/auth/signin">
               {/* Login page goes here  */}
             </Route>
             <Route exact path="/auth/signup">
               {/* Signup page goes here  */}
+              {/* <SignUpPage /> */}
             </Route>
             <Route exact path="/add-item">
               {/* Add item page goes here  */}
