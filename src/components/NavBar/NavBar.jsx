115937114f0247cf1786e81f6477fb1545bea1d2
Abdulrahman Y. idlbi <adlogi@alum.mit.edu>
2021-02-27 12:22:04 +0300

    style: remove redundant import

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import i18n from '../../i18n';
 import { Link } from 'react-router-dom';
 import {
   Menu,
