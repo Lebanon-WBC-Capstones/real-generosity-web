7dd884ce127cdb4bbe0fcb9d7a7aa81c83e9841e
Skia69 <Skiastep@gmail.com>
2021-03-02 16:18:33 +0200

    feat: barebones implementation

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,28 @@
-import firebase from 'firebase';
-import '@firebase/firestore';
+import dotenv from 'dotenv';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC4edsRlUvSF_Cu26vsIHVAZI7KBvsJDYA',
-  authDomain: 'real-generosity-3916e.firebaseapp.com',
-  projectId: 'real-generosity-3916e',
-  storageBucket: 'real-generosity-3916e.appspot.com',
-  messagingSenderId: '237589675652',
-  appId: '1:237589675652:web:d723ac74d3d9b31c758cd1',
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAVeBz1hF0f1LHH0G3FaiajieevZPUDs6s',
+  authDomain: 'real-generosity-app.firebaseapp.com',
+  projectId: 'real-generosity-app',
+  storageBucket: 'real-generosity-app.appspot.com',
+  messagingSenderId: '180908185202',
+  appId: '1:180908185202:web:f92d35f0c0660f412354eb',
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-export default firebaseConfig;
+export default firebase;
 
-//   export default firebase.firestore();
-//   export const auth=firebase.auth();
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
