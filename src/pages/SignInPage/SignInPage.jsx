3fa4ed77b253e46e185d27a4315b11fa981f2e2c
Skia69 <Skiastep@gmail.com>
2021-03-03 16:19:04 +0200

    refac: login flow initial setup

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useHistory } from 'react-router-dom';
 import {
   Box,
   Image,
@@ -11,20 +12,27 @@ import {
   Button,
 } from '@chakra-ui/react';
 import proto from '../../assets/images/proto.png';
-import GoogleButton from 'react-google-button';
 import { useTranslation } from 'react-i18next';
+import { auth } from '../../firebaseConfig';
 
 function SignInPage() {
   const { t } = useTranslation();
-
+  const history = useHistory();
   const [authEmail, setAuthEmail] = useState('');
-  const handleAuthEmailChange = (e) => {
-    setAuthEmail(e.target.value);
-  };
-
   const [authPassword, setAuthPassword] = useState('');
-  const handleAuthPasswordChange = (e) => {
-    setAuthPassword(e.target.value);
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(authEmail, authPassword);
+    if (user) {
+      history.push('/');
+    }
   };
 
   return (
@@ -51,59 +59,51 @@ function SignInPage() {
         <Flex minH="80vh" align="center" justify="space-between">
           <Box fontSize="4xl">{t('signin.paragraph')}</Box>
           <Box>
-            <Box mt={4} fontSize="lg">
-              <Text mb={2}>{t('signin.email')}</Text>
-              <Input
-                value={authEmail}
-                onChange={handleAuthEmailChange}
-                type="email"
-                size="sm"
-                variant="filled"
-                isRequired
-                maxWidth={72}
-                focusBorderColor="green.200"
-              />
-            </Box>
+            <form onSubmit={handleSubmit}>
+              <Box mt={4} fontSize="lg">
+                <Text mb={2}>{t('signin.email')}</Text>
+                <Input
+                  value={authEmail}
+                  onChange={(e) => setAuthEmail(e.target.value)}
+                  type="email"
+                  size="sm"
+                  variant="filled"
+                  isRequired
+                  maxWidth={72}
+                  focusBorderColor="green.200"
+                />
+              </Box>
+              <Box mt={8} fontSize="lg">
+                <Text mb={2}>{t('signin.password')}</Text>
+                <Input
+                  value={authPassword}
+                  onChange={(e) => setAuthPassword(e.target.value)}
+                  type="password"
+                  size="sm"
+                  variant="filled"
+                  isRequired
+                  maxWidth={72}
+                  focusBorderColor="green.200"
+                />
+              </Box>
 
-            <Box mt={8} fontSize="lg">
-              <Text mb={2}>{t('signin.password')}</Text>
-              <Input
-                value={authPassword}
-                onChange={handleAuthPasswordChange}
-                type="password"
-                size="sm"
-                variant="filled"
-                isRequired
-                maxWidth={72}
-                focusBorderColor="green.200"
-              />
-            </Box>
-            <Box mt={8}>
-              <Text
-                as="em"
-                color="blue.500"
-                fontSize="md"
-                _hover={{ color: 'blue', cursor: 'pointer' }}
-              >
-                {t('signin.forgotyourpassword')}
-              </Text>
-            </Box>
+              <Box mt={8}>
+                <Text
+                  as="em"
+                  color="blue.500"
+                  fontSize="md"
+                  _hover={{ color: 'blue', cursor: 'pointer' }}
+                >
+                  {t('signin.forgotyourpassword')}
+                </Text>
+              </Box>
 
-            <Box mt={8}>
-              <Button colorScheme="green" w={72}>
-                {t('signin.signinbutton')}
-              </Button>
-            </Box>
-
-            <Box mt={8}>
-              <GoogleButton
-                type="light"
-                boxShadow="green"
-                onClick={() => {
-                  alert('Google button clicked');
-                }}
-              />
-            </Box>
+              <Box mt={8}>
+                <Button type="submit" colorScheme="green" w={72}>
+                  {t('signin.signinbutton')}
+                </Button>
+              </Box>
+            </form>
           </Box>
         </Flex>
       </GridItem>
