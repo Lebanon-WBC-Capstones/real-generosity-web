7371763c0a55a5d84faebebe826596497109c448
Skia69 <Skiastep@gmail.com>
2021-03-04 19:40:02 +0200

    refac: redundant useQuery

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,22 +3,19 @@ import { Box } from '@chakra-ui/react';
 import HeroSection from '../../components/HeroSection';
 import CategoryCardLayout from '../../components/CategoryCardsLayout';
 import CardList from '../../components/CardList';
-function HomePage({ setQuery }) {
+function HomePage() {
   return (
-    
-      <Box>
-          <HeroSection />
+    <Box>
+      <HeroSection />
 
-        <Box mx="auto" my="36">
-          <CategoryCardLayout setQuery={setQuery} />
-        </Box>
-
-        <Box mb="20" mx="56">
-          <CardList />
-        </Box>
-        
+      <Box mx="auto" my="36">
+        <CategoryCardLayout />
       </Box>
-    
+
+      <Box mb="20" mx="56">
+        <CardList />
+      </Box>
+    </Box>
   );
 }
 
