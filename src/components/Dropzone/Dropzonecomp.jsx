f67ade13aa579f46730d784ca01645791710c9a2
Skia69 <Skiastep@gmail.com>
2021-03-04 15:55:09 +0200

    refac: dropzone now works properly

--- src/components/Dropzone/Dropzonecomp.jsx
+++ src/components/Dropzone/Dropzonecomp.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import './Dropzonecomp.css';
 import { Plus } from 'react-feather';
 import { Icon } from '@chakra-ui/react';
-function Dropzonecomp() {
+
+function Dropzonecomp({ dropzoneRef, setImage }) {
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      // Do something with the files
+      setImage(acceptedFiles[0].path);
+    },
+    [setImage]
+  );
+
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 3,
     accept: 'image/*,.pdf',
+    onDrop,
   });
 
   return (
     <section className="container">
       <div {...getRootProps({ className: 'dropzone' })}>
-        <input {...getInputProps()} className="dropzone" />
-
+        <input
+          name="image"
+          {...getInputProps()}
+          ref={dropzoneRef}
+          className="dropzone"
+        />
         <Icon my="auto" as={Plus} />
       </div>
     </section>
