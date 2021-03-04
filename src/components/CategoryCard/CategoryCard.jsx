0ed3b0c3a74e04902fa4d9335851e80108df7df9
Skia69 <Skiastep@gmail.com>
2021-03-04 19:38:50 +0200

    refac: remove queryString

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,32 +1,29 @@
+import { Flex, Image, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Flex, Stack, Text, Image } from '@chakra-ui/react';
 
 const CategoryCard = ({ categoryPic, direction, categoryName, ml, mt }) => {
   return (
-    <Link to={`/items?category=${categoryName.toLowerCase()}`}>
-      <Flex
-        mb="4"
-        fontSize="md"
-        Width="100%"
-        height="100%"
-        justifyContent="center"
-        _hover={{ cursor: 'pointer' }}
-      >
-        <Stack direction={direction} mb="4">
-          <Image src={categoryPic} maxW="90%" maxH="90%" m="auto" />
+    <Flex
+      mb="4"
+      fontSize="md"
+      Width="100%"
+      height="100%"
+      justifyContent="center"
+      _hover={{ cursor: 'pointer' }}
+    >
+      <Stack direction={direction} mb="4">
+        <Image src={categoryPic} maxW="90%" maxH="90%" m="auto" />
 
-          <Flex mt={mt} justify="left" spacing="2">
-            <Text fontWeight={600} ml={ml} mr="2">
-              {categoryName}
-            </Text>
-            <Text mr="1" color="gray.400">
-              55
-            </Text>
-          </Flex>
-        </Stack>
-      </Flex>
-    </Link>
+        <Flex mt={mt} justify="left" spacing="2">
+          <Text fontWeight={600} ml={ml} mr="2">
+            {categoryName}
+          </Text>
+          <Text mr="1" color="gray.400">
+            55
+          </Text>
+        </Flex>
+      </Stack>
+    </Flex>
   );
 };
 
