1fbf654a5dfeafb99998675657e5ed065e652c6d
Skia69 <Skiastep@gmail.com>
2021-03-04 16:52:24 +0200

    bug: firebase configs moved to env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,20 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-
-  //My configs
-  apiKey: 'AIzaSyAVeBz1hF0f1LHH0G3FaiajieevZPUDs6s',
-  authDomain: 'real-generosity-app.firebaseapp.com',
-  projectId: 'real-generosity-app',
-  storageBucket: 'real-generosity-app.appspot.com',
-  messagingSenderId: '180908185202',
-  appId: '1:180908185202:web:cae2fb7a165e3d212354eb',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
