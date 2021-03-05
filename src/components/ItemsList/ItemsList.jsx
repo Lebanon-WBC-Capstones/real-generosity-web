3ced592db4ae53b4546493ec6c447ecea87e7ba2
Skia69 <Skiastep@gmail.com>
2021-03-05 18:09:50 +0200

    fix: querying data now happens at the page level

--- src/components/ItemsList/ItemsList.jsx
+++ src/components/ItemsList/ItemsList.jsx
@@ -1,22 +1,13 @@
-import React from 'react';
 import { Box, Grid } from '@chakra-ui/react';
+import React from 'react';
 import Card from '../Card';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { firestore } from '../../services/firebase';
 
-const ItemsList = () => {
-   const items = firestore.collection('items')
-   const [itemslist,loading,error]=useCollectionData(items)
-     console.log(loading)
-     console.log("items",itemslist)
-     if (error) console.error(error)
-     if (loading) return <>loading ...</>
+const ItemsList = ({ items }) => {
   return (
     <Box maxWidth="540px" bg="white" borderRadius="1px">
       <Grid templateColumns="repeat(2, 0.5fr)" gap={1} my="4px">
-        { 
-        itemslist.map((item) => (
-          <Card key={Date.now() + '' + Math.random()} {...item} />
+        {items.map((item) => (
+          <Card key={item.id} id={item.id} {...item.data()} />
         ))}
       </Grid>
     </Box>
