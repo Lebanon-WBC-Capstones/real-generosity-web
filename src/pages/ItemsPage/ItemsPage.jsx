1fa6ac376f51b96b535f8692a39d0636fd5542ea
Skia69 <Skiastep@gmail.com>
2021-03-05 18:10:26 +0200

    fix: querying items now happens at the page level

--- src/pages/ItemsPage/ItemsPage.jsx
+++ src/pages/ItemsPage/ItemsPage.jsx
@@ -1,23 +1,32 @@
+import { Box, Container, Flex } from '@chakra-ui/react';
 import React from 'react';
-import { Container, Flex, Box } from '@chakra-ui/react';
-import ItemsMap from '../../components/ItemsMap/ItemsMap';
-import Header from '../../components/Header';
 import Category from '../../components/Category';
+import Header from '../../components/Header';
 import ItemsList from '../../components/ItemsList';
+import ItemsMap from '../../components/ItemsMap/ItemsMap';
+import { firestore } from '../../services/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 const ItemsPage = () => {
+  const query = firestore.collection('items');
+  const [items, loading, error] = useCollection(query);
+
+  if (error) return 'an error has occured...';
+
   return (
     <Container my="45px" maxW="1080px">
       <Box mb="45px">
         <Header />
       </Box>
+
       <Box mb="45px">
         <Category />
       </Box>
 
       <Flex justify="space-between">
         <Box w="50%">
-          <ItemsList />
+          {loading && 'loading...'}
+          {items && <ItemsList items={items.docs} />}
         </Box>
         <Box w="50%" ml={30}>
           <ItemsMap />
