a969d1089724d2ed526fe3d5bb72f0833c981b13
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-03-05 15:05:06 +0200

    feat: firebase calls for data

--- src/components/ItemsList/ItemsList.jsx
+++ src/components/ItemsList/ItemsList.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Box, Grid } from '@chakra-ui/react';
-
 import Card from '../Card';
-
-import { items } from '../../assets/data/items';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { firestore } from '../../services/firebase';
 
 const ItemsList = () => {
+   const items = firestore.collection('items')
+   const [itemslist,loading,error]=useCollectionData(items)
+     console.log(loading)
+     console.log("items",itemslist)
+     if (error) console.error(error)
+     if (loading) return <>loading ...</>
   return (
     <Box maxWidth="540px" bg="white" borderRadius="1px">
       <Grid templateColumns="repeat(2, 0.5fr)" gap={1} my="4px">
-        {items.map((item) => (
+        { 
+        itemslist.map((item) => (
           <Card key={Date.now() + '' + Math.random()} {...item} />
         ))}
       </Grid>
