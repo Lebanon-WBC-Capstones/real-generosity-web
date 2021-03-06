6d97cbcac94583f286f7f75f3439716722b11588
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-03-06 11:55:16 +0200

    feat : add contactUs button functionality

--- src/pages/ContactUsPage/ContactUsPage.jsx
+++ src/pages/ContactUsPage/ContactUsPage.jsx
@@ -13,24 +13,27 @@ import {
 } from '@chakra-ui/react';
 import proto from '../../assets/images/proto.png';
 import { useTranslation } from 'react-i18next';
+import emailjs from 'emailjs-com';
 
-function ContactUsPage() {
+ function ContactUsPage() {
   const { t } = useTranslation();
 
-  const [fullName, setFullName] = useState('');
-  const handleFullNameChange = (e) => {
-    setFullName(e.target.value);
-  };
+  const sendEmail=(e)=>{
+    e.preventDefault();
 
-  const [emailAddress, setEmailAddress] = useState('');
-  const handleEmailAddressChange = (e) => {
-    setEmailAddress(e.target.value);
-  };
+     emailjs.sendForm('real_generosity', 'template_in561cn', e.target, 'user_0AZ26wMMAKxuuS0KdvVRX')
 
-  const [message, setMessage] = useState('');
-  const handleMessageChange = (e) => {
-    setMessage(e.target.value);
-  };
+    .then((result) => {
+        alert(`${t('contactUs.alert')}`);
+       
+    }, (error) => {
+        alert(error.message)
+        
+    });
+    e.target.reset()
+
+
+}
   return (
     <Grid templateColumns="repeat(3, 1fr)" gap={4} fontFamily="Montserrat">
       <GridItem colSpan={1}>
@@ -57,50 +60,50 @@ function ContactUsPage() {
         <Flex minH="80vh" align="center" justify="space-between">
           <Box fontSize="4xl">{t('contactUs.paragraph')}</Box>
           <Box>
+            <form  onSubmit={sendEmail}>
             <Box mt={4} fontSize="lg">
               <Text mb={2}>{t('contactUs.fullname')}</Text>
               <Input
-                value={fullName}
-                onChange={handleFullNameChange}
+                type="text"
                 size="sm"
                 variant="filled"
                 isRequired
                 focusBorderColor="green.200"
                 maxWidth={72}
+                name="name"
               />
             </Box>
 
             <Box mt={8} fontSize="lg">
               <Text mb={2}>{t('contactUs.email')}</Text>
               <Input
-                value={emailAddress}
-                onChange={handleEmailAddressChange}
                 type="email"
                 size="sm"
                 variant="filled"
                 isRequired
                 maxWidth={72}
                 focusBorderColor="green.200"
+                name="email"
               />
             </Box>
             <Box mt={8} fontSize="lg">
               <Text mb={2}>{t('contactUs.message')}</Text>
               <Textarea
-                value={message}
-                onChange={handleMessageChange}
                 size="sm"
                 variant="filled"
                 isRequired
                 maxWidth={72}
                 focusBorderColor="green.200"
+                name="message"
               />
             </Box>
 
             <Box mt={8}>
-              <Button colorScheme="green" w={72}>
+              <Button type="submit" value="send" colorScheme="green" w={72} type="submit">
                 {t('contactUs.send')}
               </Button>
             </Box>
+            </form>
           </Box>
         </Flex>
       </GridItem>
