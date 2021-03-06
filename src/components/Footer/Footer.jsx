6c15fab56d0583c91c7a4e2820d1c1566ad6b7cc
sally-al-sardouk <sally.sardouk98@gmail.com>
2021-03-06 10:39:45 +0200

    feat footer responsive ch

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,24 +3,32 @@ import { Box, Flex, HStack, Text } from '@chakra-ui/react';
 import { Facebook, Instagram, GitHub, Twitter } from 'react-feather';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-
-const Footer = () => {
+const Footer = (props) => {
   const { t } = useTranslation();
+  
   return (
-    <Box
+    <Flex
+   
       py="10"
       as="footer"
       justify="space-between"
-      fontSize={16}
+      fontSize={["xx-small","xs","sm", "xl"]} 
+     
       fontWeight={600}
       bg="gray.50"
+      width="100%"
+     direction="column"
+    
+     {...props}
     >
       <HStack
-        spacing={16}
+        
         mb="10"
-        justify="center"
+        justify="space-between"
         color="gray.500"
         _hover={{ cursor: 'pointer' }}
+        align="center"
+        px={["10","48","52","60"]}
       >
         <Link to="/">
           {' '}
@@ -38,19 +46,19 @@ const Footer = () => {
           <Text>{t('navbar.contactUs')}</Text>
         </Link>
       </HStack>
-      <HStack mb="10" spacing={50} justify="center">
-        <Box color="gray.500" fontSize="sm">
+      <HStack mb="10" justify="space-between"  px={["12","10","56","80"]} >
+        <Box color="gray.500" fontSize="xs">
           <a href="http://www.facebook.com">
             {' '}
             <Facebook />
           </a>
         </Box>
-        <Box color="gray.500" fontSize="sm">
+        <Box color="gray.500" fontSize="xs">
           <a href="http://www.instagram.com">
-            <Instagram />
+            <Instagram  />
           </a>
         </Box>
-        <Box color="gray.500" fontSize="sm">
+        <Box color="gray.500" fontSize="xl">
           <a href="http://www.github.com">
             <GitHub />
           </a>
@@ -65,7 +73,7 @@ const Footer = () => {
       <Flex mr="10" justify="flex-end" color="gray.500">
         &copy; {t('navbar.copy')}
       </Flex>
-    </Box>
+    </Flex>
   );
 };
 
