ec27ef06a711490a6b0a1cbe8be4c60d0074e969
Skia69 <Skiastep@gmail.com>
2021-03-08 00:59:20 +0200

    bug: this error took an hour to catch XD

--- src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
+++ src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
@@ -1,14 +1,13 @@
+import { Flex, Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
-import { Grid, GridItem, Flex } from '@chakra-ui/react';
-import CategoryCard from '../CategoryCard';
-import sofa from '../../assets/images/sofa.png';
-import Clothes from '../../assets/images/Clothes.png';
-import book from '../../assets/images/book.png';
-import toys from '../../assets/images/toys.png';
-import medicalkit from '../../assets/images/medicalkit.png';
-import appliances from '../../assets/images/appliances.png';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import appliances from '../../assets/images/appliances.png';
+import book from '../../assets/images/book.png';
+import Clothes from '../../assets/images/Clothes.png';
+import medicalkit from '../../assets/images/medicalkit.png';
+import sofa from '../../assets/images/sofa.png';
+import toys from '../../assets/images/toys.png';
+import CategoryCard from '../CategoryCard';
 
 const CategoryCardsLayout = () => {
   const { t } = useTranslation();
@@ -23,62 +22,50 @@ const CategoryCardsLayout = () => {
         gap={4}
       >
         <GridItem borderRadius="xl" colSpan={8} rowSpan={12} bg="#EAEAF1">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={sofa}
-              direction="column"
-              categoryName={t('categoryCards.furniture')}
-              ml="4"
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={sofa}
+            direction="column"
+            categoryName={t('categoryCards.furniture')}
+            ml="4"
+          ></CategoryCard>
         </GridItem>
         <GridItem borderRadius="xl" colSpan={5} rowSpan={6} bg="#F6E8CD">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={Clothes}
-              direction="column"
-              categoryName={t('categoryCards.clothes')}
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={Clothes}
+            direction="column"
+            categoryName={t('categoryCards.clothes')}
+          ></CategoryCard>
         </GridItem>
         <GridItem borderRadius="xl" colSpan={5} rowSpan={6} bg="#CBECE9">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={book}
-              direction="column"
-              categoryName={t('categoryCards.books')}
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={book}
+            direction="column"
+            categoryName={t('categoryCards.books')}
+          ></CategoryCard>
         </GridItem>
         <GridItem borderRadius="xl" colSpan={7} rowSpan={6} bg="#E6D0EF">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={toys}
-              direction="row-reverse"
-              categoryName={t('categoryCards.toys')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={toys}
+            direction="row-reverse"
+            categoryName={t('categoryCards.toys')}
+            mt="24"
+          ></CategoryCard>
         </GridItem>
         <GridItem borderRadius="xl" colSpan={7} rowSpan={6} bg="#F0D0D2">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={medicalkit}
-              direction="row-reverse"
-              categoryName={t('categoryCards.medics')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={medicalkit}
+            direction="row-reverse"
+            categoryName={t('categoryCards.medics')}
+            mt="24"
+          ></CategoryCard>
         </GridItem>
         <GridItem borderRadius="xl" colSpan={10} rowSpan={6} bg="#CFD6F2">
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={appliances}
-              direction="row-reverse"
-              categoryName={t('categoryCards.appliances')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
+          <CategoryCard
+            categoryPic={appliances}
+            direction="row-reverse"
+            categoryName={t('categoryCards.appliances')}
+            mt="24"
+          ></CategoryCard>
         </GridItem>
       </Grid>
     </Flex>
