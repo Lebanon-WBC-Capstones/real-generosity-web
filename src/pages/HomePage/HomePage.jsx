8eb8213a0cc475d510da3ca6eeafd9c53770b851
Skia69 <Skiastep@gmail.com>
2021-03-08 00:47:51 +0200

    refac: fix items collection query

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,22 +3,28 @@ import { Box, Flex } from '@chakra-ui/react';
 import HeroSection from '../../components/HeroSection';
 import CategoryCardLayout from '../../components/CategoryCardsLayout';
 import CardList from '../../components/CardList';
-function HomePage({ setQuery }) {
-  return (
+import { useCollectionOnce } from 'react-firebase-hooks/firestore';
+import { firestore } from '../../services/firebase';
 
+function HomePage() {
+  const query = firestore.collection('items').limit(4);
+  const [items, loading, error] = useCollectionOnce(query);
+
+  if (error) return 'an error has occured...';
+
+  return (
     <Flex direction="column" justify="space-evenly">
       <HeroSection />
 
       <Box my="24" justify="center" mx="auto">
-        <CategoryCardLayout setQuery={setQuery} />
+        <CategoryCardLayout />
       </Box>
 
       <Box mb="14" justify="center" mx="auto">
-        <CardList />
+        {loading && 'items list is loading...'}
+        {items && <CardList items={items.docs} />}
       </Box>
-
     </Flex>
-
   );
 }
 
