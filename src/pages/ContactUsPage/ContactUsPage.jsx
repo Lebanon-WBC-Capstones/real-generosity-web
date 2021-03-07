93ba68a41b449ad4f03db71ffc61efd212903d20
Skia69 <Skiastep@gmail.com>
2021-03-08 00:04:59 +0200

    fix: fix warnings

--- src/pages/ContactUsPage/ContactUsPage.jsx
+++ src/pages/ContactUsPage/ContactUsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   Box,
@@ -15,25 +15,30 @@ import proto from '../../assets/images/proto.png';
 import { useTranslation } from 'react-i18next';
 import emailjs from 'emailjs-com';
 
- function ContactUsPage() {
+function ContactUsPage() {
   const { t } = useTranslation();
 
-  const sendEmail=(e)=>{
+  const sendEmail = (e) => {
     e.preventDefault();
 
-     emailjs.sendForm('real_generosity', 'template_in561cn', e.target, 'user_0AZ26wMMAKxuuS0KdvVRX')
+    emailjs
+      .sendForm(
+        'real_generosity',
+        'template_in561cn',
+        e.target,
+        'user_0AZ26wMMAKxuuS0KdvVRX'
+      )
 
-    .then((result) => {
-        alert(`${t('contactUs.alert')}`);
-       
-    }, (error) => {
-        alert(error.message)
-        
-    });
-    e.target.reset()
-
-
-}
+      .then(
+        (result) => {
+          alert(`${t('contactUs.alert')}`);
+        },
+        (error) => {
+          alert(error.message);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <Grid templateColumns="repeat(3, 1fr)" gap={4} fontFamily="Montserrat">
       <GridItem colSpan={1}>
@@ -60,49 +65,49 @@ import emailjs from 'emailjs-com';
         <Flex minH="80vh" align="center" justify="space-between">
           <Box fontSize="4xl">{t('contactUs.paragraph')}</Box>
           <Box>
-            <form  onSubmit={sendEmail}>
-            <Box mt={4} fontSize="lg">
-              <Text mb={2}>{t('contactUs.fullname')}</Text>
-              <Input
-                type="text"
-                size="sm"
-                variant="filled"
-                isRequired
-                focusBorderColor="green.200"
-                maxWidth={72}
-                name="name"
-              />
-            </Box>
+            <form onSubmit={sendEmail}>
+              <Box mt={4} fontSize="lg">
+                <Text mb={2}>{t('contactUs.fullname')}</Text>
+                <Input
+                  type="text"
+                  size="sm"
+                  variant="filled"
+                  isRequired
+                  focusBorderColor="green.200"
+                  maxWidth={72}
+                  name="name"
+                />
+              </Box>
 
-            <Box mt={8} fontSize="lg">
-              <Text mb={2}>{t('contactUs.email')}</Text>
-              <Input
-                type="email"
-                size="sm"
-                variant="filled"
-                isRequired
-                maxWidth={72}
-                focusBorderColor="green.200"
-                name="email"
-              />
-            </Box>
-            <Box mt={8} fontSize="lg">
-              <Text mb={2}>{t('contactUs.message')}</Text>
-              <Textarea
-                size="sm"
-                variant="filled"
-                isRequired
-                maxWidth={72}
-                focusBorderColor="green.200"
-                name="message"
-              />
-            </Box>
+              <Box mt={8} fontSize="lg">
+                <Text mb={2}>{t('contactUs.email')}</Text>
+                <Input
+                  type="email"
+                  size="sm"
+                  variant="filled"
+                  isRequired
+                  maxWidth={72}
+                  focusBorderColor="green.200"
+                  name="email"
+                />
+              </Box>
+              <Box mt={8} fontSize="lg">
+                <Text mb={2}>{t('contactUs.message')}</Text>
+                <Textarea
+                  size="sm"
+                  variant="filled"
+                  isRequired
+                  maxWidth={72}
+                  focusBorderColor="green.200"
+                  name="message"
+                />
+              </Box>
 
-            <Box mt={8}>
-              <Button type="submit" value="send" colorScheme="green" w={72} type="submit">
-                {t('contactUs.send')}
-              </Button>
-            </Box>
+              <Box mt={8}>
+                <Button type="submit" value="send" colorScheme="green" w={72}>
+                  {t('contactUs.send')}
+                </Button>
+              </Box>
             </form>
           </Box>
         </Flex>
