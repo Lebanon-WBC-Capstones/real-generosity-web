1e7e507dfe110a369f555845ef7c8efcf7997166
Skia69 <Skiastep@gmail.com>
2021-03-09 14:34:00 +0200

    refac: remove redundant code

--- src/components/ItemsList/ItemsList.jsx
+++ src/components/ItemsList/ItemsList.jsx
@@ -2,8 +2,7 @@ import { Box, Grid } from '@chakra-ui/react';
 import React from 'react';
 import Card from '../Card';
 
-const ItemsList = ({ items, setItemsCounter }) => {
-  setItemsCounter(items.length);
+const ItemsList = ({ items }) => {
   return (
     <Box maxWidth="540px" bg="white" borderRadius="1px">
       <Grid templateColumns="repeat(2, 0.5fr)" gap={1} my="4px">
