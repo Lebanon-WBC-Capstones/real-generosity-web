c0433a1d4d8c1c2070bf3cd4e42e016a64497a7f
Skia69 <Skiastep@gmail.com>
2021-03-09 14:35:09 +0200

    refac: remove redundant code

--- src/pages/ItemsPage/ItemsPage.jsx
+++ src/pages/ItemsPage/ItemsPage.jsx
@@ -1,29 +1,16 @@
 import { Box, Container, Flex, Skeleton } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React from 'react';
 import { useCollectionOnce } from 'react-firebase-hooks/firestore';
 import Filters from '../../components/Filters';
 import Header from '../../components/Header';
+import ItemsMap from '../../components/ItemsMap';
 import ItemsList from '../../components/ItemsList';
-import ItemsMap from '../../components/ItemsMap/ItemsMap';
 import { firestore } from '../../services/firebase';
 
 const itemsRef = firestore.collection('items');
 
 const ItemsPage = () => {
-  const [categoryName, setCategoryName] = useState('All');
-  const [searchInput, setSearchInput] = useState('');
-  const [itemsCounter, setItemsCounter] = useState(0);
   const [items, loading, error] = useCollectionOnce(itemsRef);
-
-  const handleSearchChange = (e) => {
-    setSearchInput(e.target.value);
-  };
-
-  const query = itemsRef.where('category', '==', categoryName.toLowerCase());
-  const [filteredItems, catLoading] = useCollectionOnce(query);
-
-  const searchQuery = itemsRef.where('title', '==', searchInput);
-  const [filteredItemsBySearch, searchLoading] = useCollectionOnce(searchQuery);
 
   const renders = React.useRef(0);
   console.log('ItemsPage.jsx render... ', renders.current++);
@@ -43,67 +30,22 @@ const ItemsPage = () => {
     );
 
   return (
-    <Container my="45px" maxW="1080px" fontFamily="Montserrat">
+    <Container my="45px" maxW="1080px">
       <Box mb="45px">
-        <Header
-          categoryName={categoryName}
-          searchInput={searchInput}
-          handleSearchChange={handleSearchChange}
-          itemsCounter={itemsCounter}
-        />
+        <Header items={items.docs.length} />
       </Box>
 
       <Box mb="45px">
-        <Filters setCategoryName={setCategoryName} />
+        <Filters />
       </Box>
 
       <Flex justify="space-between">
         <Box w="50%">
-          {searchInput ? (
-            (filteredItemsBySearch && (
-              <ItemsList
-                items={filteredItemsBySearch.docs}
-                setItemsCounter={setItemsCounter}
-              />
-            )) ||
-            (searchLoading && `loading item: ${searchInput}`)
-          ) : categoryName === 'All' ? (
-            <ItemsList items={items.docs} setItemsCounter={setItemsCounter} />
-          ) : (
-            (filteredItems && (
-              <ItemsList
-                items={filteredItems.docs}
-                setItemsCounter={setItemsCounter}
-              />
-            )) ||
-            (catLoading && `loading category: ${categoryName}`)
-          )}
-
-          {/* non filtered categories */}
-          {/* {categoryName === 'All' && <ItemsList items={items.docs} />} */}
-
-          {/* filtered categories  */}
-          {/* {filteredItems && <ItemsList items={filteredItems.docs} />} */}
-          {/* {catLoading && `loading category: ${categoryName}`} */}
+          <ItemsList items={items.docs} />
         </Box>
-        <Box w="50%" ml={30}>
-          {searchInput ? (
-            filteredItemsBySearch && (
-              <ItemsMap items={filteredItemsBySearch.docs} />
-            )
-          ) : categoryName === 'All' ? (
-            <ItemsMap items={items.docs} />
-          ) : (
-            filteredItems &&
-            categoryName !== 'All' && <ItemsMap items={filteredItems.docs} />
-          )}
-          {/* non filtered categories */}
-          {/* {categoryName === 'All' && <ItemsMap items={items.docs} />} */}
 
-          {/* filtered categories  */}
-          {/* {filteredItems && categoryName !== 'All' && (
-            <ItemsMap items={filteredItems.docs} />
-          )} */}
+        <Box w="50%" ml={30}>
+          <ItemsMap items={items.docs} />
         </Box>
       </Flex>
     </Container>
