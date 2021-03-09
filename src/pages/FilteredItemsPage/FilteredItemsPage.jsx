f10994a0e509cd16954b72446c23d229d70b5efc
Skia69 <Skiastep@gmail.com>
2021-03-09 14:34:26 +0200

    feat: add separate page for filtered items

---
+++ src/pages/FilteredItemsPage/FilteredItemsPage.jsx
@@ -0,0 +1,62 @@
+import { Box, Container, Flex, Skeleton } from '@chakra-ui/react';
+import React from 'react';
+import { useCollectionOnce } from 'react-firebase-hooks/firestore';
+import { useParams } from 'react-router-dom';
+import Filters from '../../components/Filters';
+import Header from '../../components/Header';
+import ItemsList from '../../components/ItemsList';
+import ItemsMap from '../../components/ItemsMap/ItemsMap';
+import { firestore } from '../../services/firebase';
+
+const itemsRef = firestore.collection('items');
+
+const FilteredItemsPage = () => {
+  const { category } = useParams();
+
+  const renders = React.useRef(0);
+  console.log('FilteredItemsPage.jsx render... ', renders.current++);
+
+  const query = itemsRef.where('category', '==', category);
+  const [items, loading, error] = useCollectionOnce(query);
+
+  if (error) return 'an error has occured...';
+
+  if (loading)
+    return (
+      <Container centerContent minH="100vh">
+        <Skeleton w={1080} my={50} h={200} />
+        <Skeleton w={1080} mb={50} h={50} />
+        <Flex>
+          <Skeleton mr={30} w={500} h="80vh" />
+          <Skeleton w={500} h="80vh" />
+        </Flex>
+      </Container>
+    );
+
+  return (
+    <Container my="45px" maxW="1080px">
+      <Box mb="45px">
+        <Header filteredCategoryCount={items.docs.length} />
+      </Box>
+
+      <Box mb="45px">
+        <Filters />
+      </Box>
+
+      <Flex justify="space-between">
+        <Box w="50%">
+          {items.docs.length ? (
+            <ItemsList items={items.docs} />
+          ) : (
+            'no match found...'
+          )}
+        </Box>
+        <Box w="50%" ml={30}>
+          <ItemsMap items={items.docs} />
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
+
+export default FilteredItemsPage;
