d3bdd39eade7f914270f5b8fd2ba60edb42f949f
fatima-katrib <73742127+fatima-katrib@users.noreply.github.com>
2021-03-09 11:10:49 +0200

    Fix: change categ cards

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Flex, Stack, Text, Image } from '@chakra-ui/react';
+import { Flex, Box, Text, Image } from '@chakra-ui/react';
 
-const CategoryCard = ({ categoryPic, direction, categoryName, ml, mt }) => {
+const CategoryCard = ({ categoryPic, categoryName, bgColor }) => {
   return (
     <Link to={`/items?category=${categoryName.toLowerCase()}`}>
       <Flex
+        direction="column"
+        justifyContent="space-between"
+        align="center"
+        borderRadius="xl"
+        w="32"
+        h="48"
+        py="8"
+        boxShadow="lg"
+        fontFamily="Montserrat"
+      >
+        <Image src={categoryPic} maxW="10" />
+        <Text m="auto" fontWeight="bold">{categoryName}</Text>
+        <Box
+          bgColor={bgColor}
+          w="10"
+          h="10"
+          align="center"
+          justifyContent="center"
+          m="auto"
+          borderRadius="lg" >
+          <Text fontSize="xs" my="25%" textAlign="center" >56+</Text>
+        </Box>
+
+      </Flex>
+      {/* <Flex
         mb="4"
         fontSize="md"
         Width="100%"
@@ -25,7 +50,7 @@ const CategoryCard = ({ categoryPic, direction, categoryName, ml, mt }) => {
             </Text>
           </Flex>
         </Stack>
-      </Flex>
+      </Flex> */}
     </Link>
   );
 };
--- src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
+++ src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Grid, GridItem, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import CategoryCard from '../CategoryCard';
-import sofa from '../../assets/images/sofa.png';
+import furniture from '../../assets/images/furniture.png';
 import Clothes from '../../assets/images/Clothes.png';
 import book from '../../assets/images/book.png';
 import toys from '../../assets/images/toys.png';
@@ -14,109 +14,61 @@ const CategoryCardsLayout = ({ setQuery }) => {
   const { t } = useTranslation();
 
   return (
-    <Flex justifyContent="center">
-      <Grid
-        maxH="72"
-        maxW="4xl"
-        templateRows="repeat(12, 1fr)"
-        templateColumns="repeat(25, 1fr)"
-        gap={4}
-      >
-        <GridItem
+    <Flex direction="row" align="center" justifyContent="space-between" maxW="4xl" m="auto" flexWrap="wrap">
+      <Flex mr="3">
+
+        <CategoryCard
+          categoryPic={furniture}
+          categoryName={t('categoryCards.furniture')}
           onClick={() => setQuery('furniture')}
-          borderRadius="xl"
-          colSpan={8}
-          rowSpan={12}
-          bg="#EAEAF1"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={sofa}
-              direction="column"
-              categoryName={t('categoryCards.furniture')}
-              ml="4"
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-        <GridItem
+          bgColor="#EAEAF1"
+        ></CategoryCard>
+      </Flex>
+
+      <Flex mt="9" mx="3">
+        <CategoryCard
+          categoryPic={Clothes}
+          categoryName={t('categoryCards.clothes')}
           onClick={() => setQuery('clothes')}
-          borderRadius="xl"
-          colSpan={5}
-          rowSpan={6}
-          bg="#F6E8CD"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={Clothes}
-              direction="column"
-              categoryName={t('categoryCards.clothes')}
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-        <GridItem
+          bgColor="#F6E8CD"
+        ></CategoryCard>
+      </Flex>
+
+      <Flex mx="3">
+        <CategoryCard
+          categoryPic={book}
+          categoryName={t('categoryCards.books')}
           onClick={() => setQuery('books')}
-          borderRadius="xl"
-          colSpan={5}
-          rowSpan={6}
-          bg="#CBECE9"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={book}
-              direction="column"
-              categoryName={t('categoryCards.books')}
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-        <GridItem
+          bgColor="#CBECE9"
+        ></CategoryCard>
+      </Flex>
+
+      <Flex mx="3">
+        <CategoryCard
+          categoryPic={toys}
+          categoryName={t('categoryCards.toys')}
           onClick={() => setQuery('toys')}
-          borderRadius="xl"
-          colSpan={7}
-          rowSpan={6}
-          bg="#E6D0EF"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={toys}
-              direction="row-reverse"
-              categoryName={t('categoryCards.toys')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-        <GridItem
+          bgColor="#E6D0EF"
+        ></CategoryCard>
+      </Flex>
+
+      <Flex mx="3" mt="9">
+        <CategoryCard
+          categoryPic={medicalkit}
+          categoryName={t('categoryCards.medics')}
           onClick={() => setQuery('medics')}
-          borderRadius="xl"
-          colSpan={7}
-          rowSpan={6}
-          bg="#F0D0D2"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={medicalkit}
-              direction="row-reverse"
-              categoryName={t('categoryCards.medics')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-        <GridItem
+          bgColor="#F0D0D2"
+        ></CategoryCard>
+      </Flex>
+
+      <Flex ml="3">
+        <CategoryCard
+          categoryPic={appliances}
+          categoryName={t('categoryCards.appliances')}
           onClick={() => setQuery('appliances')}
-          borderRadius="xl"
-          colSpan={10}
-          rowSpan={6}
-          bg="#CFD6F2"
-        >
-          <Link to="/auth//items">
-            <CategoryCard
-              categoryPic={appliances}
-              direction="row-reverse"
-              categoryName={t('categoryCards.appliances')}
-              mt="24"
-            ></CategoryCard>
-          </Link>
-        </GridItem>
-      </Grid>
+          bgColor="#CFD6F2"
+        ></CategoryCard>
+      </Flex>
     </Flex>
   );
 };
