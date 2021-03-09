bb131e9a762be4be47261ac9bfec947202fecace
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-03-09 17:22:31 +0200

    feat: add links to the menu

--- src/components/GetStartedBtn/GetStartedBtn.jsx
+++ src/components/GetStartedBtn/GetStartedBtn.jsx
@@ -5,28 +5,29 @@ import {
   User,
   LogOut,
   Bell,
-  Gift,
-  ShoppingBag,
   AlertCircle,
 } from 'react-feather';
 import { auth } from '../../services/firebase';
 import { useAuth } from '../../contexts/AuthContext';
-import {
+import {Box,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
   Button,
-  Avatar,
+  Avatar,AvatarBadge,Badge,
   IconButton,
-  HStack,
+  HStack,CircleIcon
 } from '@chakra-ui/react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import firebase, { firestore } from '../../services/firebase';
 
 const GetStartedBtn = () => {
   const { t } = useTranslation();
   const user = useAuth();
   const history = useHistory();
   console.log('context user', user);
+
 
   const logOut = async () => {
     try {
@@ -36,24 +37,58 @@ const GetStartedBtn = () => {
       console.log('an error has occured...', error);
     }
   };
+
+  // query notifications from firebase
+//   const notifications=firestore.collection('notifications').where('targetId','==',user.uid);
+//   const [notify, notifyloading, notifyerror] = useCollectionData(notifications)
+//   console.log('notify',notify)
+
+//  if (notifyerror) console.log('error')
+//  if (notifyloading) return <>loading...</>
+//
+
+// React.useEffect(()=>{
+//   firebase
+//   .firestore
+//   .collection("notifications")
+//   .where('targetId','==',user.uid)
+//   .get()
+//   .then(function(doc) {
+//     if (doc.exists) {
+//         // setCurrentPost(doc.data());
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }})
+//   .catch(function(error) {
+//     console.log("Error getting document:", error);
+//   }), [user]
+// })
+
+// { user && notify.length!==0? <AvatarBadge boxSize="1.25em" bg="red" />:""}
+
+ 
   return (
     <>
       {user ? (
-        <HStack>
+         <HStack>
           <Menu border="1px">
             <MenuButton
               as={IconButton}
               color="black"
               aria-label="Options"
-              icon={<Bell />}
+              icon={ <Avatar size="sm" bg="white" icon={<Bell fontSize="1.5rem" />} >
+               
+                     </Avatar> }
               size="md"
               variant="ghost"
             />
-
+            
             <MenuList>
-              <MenuItem icon={<Gift />}> approval</MenuItem>
-              <MenuItem icon={<ShoppingBag />}>request</MenuItem>
-              <MenuItem icon={<AlertCircle />}> report</MenuItem>
+              <Link to={`profile/${user.uid}?q=notifications`}>
+              <MenuItem>Notifications</MenuItem>
+              </Link>
             </MenuList>
           </Menu>
 
@@ -67,9 +102,11 @@ const GetStartedBtn = () => {
               variant="ghost"
             />
             <MenuList>
-              <Link to={`/profile/${user.uid}`}>
+           
+              <Link to={`/profile/${user.uid}?q=donations`}>
                 <MenuItem icon={<User />}>{t('navbar.profile')}</MenuItem>
               </Link>
+            
               <MenuItem onClick={logOut} icon={<LogOut />}>
                 {t('navbar.logout')}
               </MenuItem>
