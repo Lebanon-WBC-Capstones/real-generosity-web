0c6198fd682e285b3be6397f3aec22a49d8fd374
Joseph <joseftaerrazi@gmai.com>
2021-03-10 21:22:41 +0200

    fix: Implement basic pagination

--- src/pages/ItemsPage/ItemsPage.jsx
+++ src/pages/ItemsPage/ItemsPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Skeleton } from '@chakra-ui/react';
+import { Box, Container, Flex, Skeleton, Button } from '@chakra-ui/react';
 import React from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import Filters from '../../components/Filters';
@@ -6,28 +6,42 @@ import Header from '../../components/Header';
 import ItemsMap from '../../components/ItemsMap';
 import ItemsList from '../../components/ItemsList';
 import { firestore } from '../../services/firebase';
+import { usePagination } from 'use-pagination-firestore';
+import firebase from 'firebase/app';
 
 const ItemsPage = () => {
   const [search, setSearch] = React.useState('');
 
-  let itemsRef = firestore
-    .collection('items')
-    // TODO: uncomment after fixing old items
-    // .where('status', '==', 'active')
-    .orderBy('createdAt', 'desc');
+  const { items, isLoading, isStart, isEnd, getPrev, getNext } = usePagination(
+    firebase.firestore().collection('items').orderBy('createdAt', 'desc'),
+    {
+      limit: 4,
+    }
+  );
+  console.log(items);
+  // let itemsRef = firestore
+  //   .collection('items')
+  //   // TODO: uncomment after fixing old items
+  //   // .where('status', '==', 'active')
+  //   .orderBy('createdAt', 'desc');
+  //   // .limit(3)
 
-  if (search) {
-    itemsRef = itemsRef.where('title', '==', search);
-  }
+  // if (search) {
+  //   itemsRef = itemsRef.where('title', '==', search);
+  // }
 
-  const [items, loading, error] = useCollection(itemsRef);
+  // const [items, loading, error] = useCollection(itemsRef);
 
-  const renders = React.useRef(0);
-  console.log('ItemsPage.jsx render... ', renders.current++);
+  // const renders = React.useRef(0);
+  // console.log('ItemsPage.jsx render... ', renders.current++);
 
-  if (error) return 'an error has occured...';
+  // if (error) return 'an error has occured...';
 
-  if (loading)
+  // const nextBatch = () => {
+  //   itemsRef = itemsRef.offset(3);
+  // };
+
+  if (isLoading)
     return (
       <Container centerContent minH="100vh">
         <Skeleton w={1080} my={50} h={200} />
@@ -42,7 +56,7 @@ const ItemsPage = () => {
   return (
     <Container my="45px" maxW="1080px">
       <Box mb="45px">
-        <Header items={items.docs.length} />
+        <Header items={items.length} />
       </Box>
 
       <Box mb="45px">
@@ -51,11 +65,17 @@ const ItemsPage = () => {
 
       <Flex justify="space-between">
         <Box w="50%">
-          <ItemsList items={items.docs} />
+          <ItemsList items={items} />
+          <Button onClick={getPrev} disabled={isStart}>
+            Prev
+          </Button>
+          <Button onClick={getNext} disabled={isEnd}>
+            Next
+          </Button>
         </Box>
 
         <Box w="50%" ml={30}>
-          <ItemsMap items={items.docs} />
+          <ItemsMap items={items} />
         </Box>
       </Flex>
     </Container>
