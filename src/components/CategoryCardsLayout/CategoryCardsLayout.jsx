0aa2262e0846a12cc8dd2fa17e8359b4eb122d71
Joseph <joseftaerrazi@gmai.com>
2021-03-18 12:29:10 +0200

    fix: translate categories header

--- src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
+++ src/components/CategoryCardsLayout/CategoryCardsLayout.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Flex, Text, Wrap, WrapItem, Stack, Center, Link } from '@chakra-ui/react';
+import {
+  Flex,
+  Text,
+  Wrap,
+  WrapItem,
+  Stack,
+  Center,
+  Link,
+} from '@chakra-ui/react';
 import CategoryCard from '../CategoryCard';
 import furniture from '../../assets/images/furniture.png';
 import Clothes from '../../assets/images/Clothes.png';
@@ -10,83 +18,84 @@ import toys from '../../assets/images/toys.png';
 import all from '../../assets/images/all.png';
 import { useTranslation } from 'react-i18next';
 
-
 const CategoryCardsLayout = () => {
   const { t } = useTranslation();
 
   return (
     <Flex maxW="4xl" m="auto" align="center" justify="space-between">
       <Stack>
-      <Text fontSize="xl" fontWeight="medium">Categories</Text>
+        <Text fontSize="xl" fontWeight="medium">
+          {t('categoryCards.header')}
+        </Text>
 
-    <Center direction="row" flexWrap="wrap" justifyContent="space-between">
-    <Wrap spacing={["","","14","16"]} align="center">
-  <WrapItem>
-        <CategoryCard
-          categoryPic={furniture}
-          categoryName={t('categoryCards.furniture')}
-          bgColor="#EAEAF1"
-          categoryLink="/items/furniture"
-        ></CategoryCard></WrapItem>
- <WrapItem>
-        <CategoryCard
-          categoryPic={Clothes}
-          categoryName={t('categoryCards.clothes')}
-          bgColor="#F6E8CD"
-          categoryLink="/items/clothes"
-        ></CategoryCard>
-        </WrapItem>
+        <Center direction="row" flexWrap="wrap" justifyContent="space-between">
+          <Wrap spacing={['', '', '14', '16']} align="center">
+            <WrapItem>
+              <CategoryCard
+                categoryPic={furniture}
+                categoryName={t('categoryCards.furniture')}
+                bgColor="#EAEAF1"
+                categoryLink="/items/furniture"
+              ></CategoryCard>
+            </WrapItem>
+            <WrapItem>
+              <CategoryCard
+                categoryPic={Clothes}
+                categoryName={t('categoryCards.clothes')}
+                bgColor="#F6E8CD"
+                categoryLink="/items/clothes"
+              ></CategoryCard>
+            </WrapItem>
 
-        <WrapItem>
-        <CategoryCard
-          categoryPic={book}
-          categoryName={t('categoryCards.books')}
-          bgColor="#CBECE9"
-          categoryLink="/items/books"
-        ></CategoryCard>
-        </WrapItem>
+            <WrapItem>
+              <CategoryCard
+                categoryPic={book}
+                categoryName={t('categoryCards.books')}
+                bgColor="#CBECE9"
+                categoryLink="/items/books"
+              ></CategoryCard>
+            </WrapItem>
 
-        <WrapItem>
-        <CategoryCard
-          categoryPic={toys}
-          categoryName={t('categoryCards.toys')}
-          bgColor="#E6D0EF"
-          categoryLink="/items/toys"
-        ></CategoryCard>
-        </WrapItem>
+            <WrapItem>
+              <CategoryCard
+                categoryPic={toys}
+                categoryName={t('categoryCards.toys')}
+                bgColor="#E6D0EF"
+                categoryLink="/items/toys"
+              ></CategoryCard>
+            </WrapItem>
 
-        <WrapItem>
-        <CategoryCard
-          categoryPic={medicalkit}
-          categoryName={t('categoryCards.medics')}
-          bgColor="#F0D0D2"
-          categoryLink="/items/medics"
-        ></CategoryCard>
-        </WrapItem>
+            <WrapItem>
+              <CategoryCard
+                categoryPic={medicalkit}
+                categoryName={t('categoryCards.medics')}
+                bgColor="#F0D0D2"
+                categoryLink="/items/medics"
+              ></CategoryCard>
+            </WrapItem>
 
-        <WrapItem>
-        <CategoryCard
-          categoryPic={appliances}
-          categoryName={t('categoryCards.appliances')}
-          bgColor="#CFD6F2"
-          categoryLink="/items/appliances"
-        ></CategoryCard>
-          </WrapItem>
+            <WrapItem>
+              <CategoryCard
+                categoryPic={appliances}
+                categoryName={t('categoryCards.appliances')}
+                bgColor="#CFD6F2"
+                categoryLink="/items/appliances"
+              ></CategoryCard>
+            </WrapItem>
 
-          <WrapItem display={{ base: "none", md: "block" }}>
-            <Link to="/items">
-        <CategoryCard
-          categoryPic={all}
-          categoryName="All"
-          bgColor="#EAEAF1"
-          categoryLink="/items"
-        ></CategoryCard>
-        </Link>
-          </WrapItem>
-</Wrap>
-
-    </Center>
-    </Stack>
+            <WrapItem display={{ base: 'none', md: 'block' }}>
+              <Link to="/items">
+                <CategoryCard
+                  categoryPic={all}
+                  categoryName="All"
+                  bgColor="#EAEAF1"
+                  categoryLink="/items"
+                ></CategoryCard>
+              </Link>
+            </WrapItem>
+          </Wrap>
+        </Center>
+      </Stack>
     </Flex>
   );
 };
