b2367e739323b620ac7aa34afa9b1e74393b41b6
Joseph <joseftaerrazi@gmai.com>
2021-03-18 13:14:40 +0200

    refac: add more shadow to cards

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -3,33 +3,36 @@ import { Link } from 'react-router-dom';
 import { Flex, Box, Text, Image } from '@chakra-ui/react';
 import { ArrowRight } from 'react-feather';
 
-const CategoryCard = ({ categoryPic, categoryName, bgColor,categoryLink }) => {
+const CategoryCard = ({ categoryPic, categoryName, bgColor, categoryLink }) => {
   return (
     <Link to={categoryLink}>
       <Flex
-
         direction="column"
         justifyContent="space-between"
         align="center"
         borderRadius="xl"
-        w={["28","36","44","44"]}
-        h={["32","40","48","48"]}
-        py={["2","4","8","8"]}
-        boxShadow="md"
+        w={['28', '36', '44', '44']}
+        h={['32', '40', '48', '48']}
+        py={['2', '4', '8', '8']}
+        boxShadow="lg"
         fontFamily="Montserrat"
       >
         <Image src={categoryPic} maxW="10" />
-        <Text m="auto" fontWeight="bold">{categoryName}</Text>
+        <Text m="auto" fontWeight="bold">
+          {categoryName}
+        </Text>
         <Box
           bgColor={bgColor}
           w="10"
           h="10"
           align="center"
           m="auto"
-          borderRadius="lg" >
-          <Flex m="2"><ArrowRight /></Flex>
+          borderRadius="lg"
+        >
+          <Flex m="2">
+            <ArrowRight />
+          </Flex>
         </Box>
-
       </Flex>
     </Link>
   );
